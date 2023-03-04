d6d9ff0156c0e67909b14eb4581d370b65186b89
nathandalehaskins <haskins.nathandale@gmail.com>
2023-03-04 15:10:15 +0200

    3.1.
    Create age_data struct and global variable my_age in main file. Successfully built and debug main.c

--- src/main.c
+++ src/main.c
@@ -9,7 +9,12 @@
 #include "lcd_stm32f0.h"
 
 // GLOBAL VARIABLES ----------------------------------------------------------|
-
+struct age_data {
+    uint8_t date;
+    uint8_t month;
+    uint16_t year; // not using uint8_t here because that would limit the years to 0-255
+    uint8_t age;
+} my_age = {28, 3, 2001, 21};
 
 // FUNCTION DECLARATIONS -----------------------------------------------------|
 
