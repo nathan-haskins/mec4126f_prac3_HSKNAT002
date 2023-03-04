707c37b35e0f56d1ef13d29295b7e780d5c13e43
nathandalehaskins <haskins.nathandale@gmail.com>
2023-03-04 15:51:04 +0200

    3.2
    Inside the main function, I created an instance of the struct with your personal birth date details. Modify the infinite loop to cause the LCD on your micro to increment until your age is displayed reach your age. I also corrected 3.1. and which created a temporary instance of the age_data struct with my personal details.

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #define STM32F051												   //COMPULSORY
 #include "stm32f0xx.h"											   //COMPULSORY
 #include "lcd_stm32f0.h"
+#include <stdio.h> // For sprintf function (3.2)
 
 // GLOBAL VARIABLES ----------------------------------------------------------|
 struct age_data {
@@ -14,7 +15,8 @@ struct age_data {
     uint8_t month;
     uint16_t year; // not using uint8_t here because that would limit the years to 0-255
     uint8_t age;
-} my_age = {28, 3, 2001, 21};
+} my_age;
+
 
 // FUNCTION DECLARATIONS -----------------------------------------------------|
 
@@ -24,12 +26,22 @@ void main(void);                                                   //COMPULSORY
 
 void main(void)
 {
+	my_age = (struct age_data){28, 3, 2001, 21};
+	init_LCD(); // INITIALISE LCD
 
+	int count = 0; // Counter variable for LCD display
 
 	while(1)
 	{
-
+        char age_string[3]; // Convert age to a string [array - one for each digit in the age, and one for the null terminator character ('\0') to mark the end of the string.]
+        sprintf(age_string, "%d", count);
+        lcd_putstring(age_string);
+        delay(120000);
+        count++;
+    	lcd_command(CLEAR);
+        if(count  > my_age.age){count = 0;}
 	}
 }
+
 
 // OTHER FUNCTIONS -----------------------------------------------------------|
